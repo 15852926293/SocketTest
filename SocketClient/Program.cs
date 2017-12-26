14bbf899c0158bdfed7745d8431f5b3c19a4ff35
WIN-TLBG1OUEOFI\admin <jiandong.zhang1@outlook.com>
2017-12-26 16:52:07 +0800

    Init SocketTest project.

---
+++ SocketClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 6000;
+            string host = "127.0.0.1";
+
+            IPAddress ip = IPAddress.Parse(host);
+            IPEndPoint ipe = new IPEndPoint(ip, port);
+
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.Connect(ipe);
+
+            //send message
+            string sendMsg = "abcdefg";
+            byte[] sendBytes = Encoding.ASCII.GetBytes(sendMsg);
+            clientSocket.Send(sendBytes);
+
+            //receive message
+            string recMsg = "";
+            byte[] recBytes = new byte[4096];
+            int bytes = clientSocket.Receive(recBytes, recBytes.Length, 0);
+            recMsg += Encoding.ASCII.GetString(recBytes, 0, bytes);
+            Console.WriteLine(recMsg);
+            //clientSocket.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SocketServer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServer
+{
+    class Program
+    {
+        public static string GetIpAddress()
+        {
+            string hostName = Dns.GetHostName();   //获取本机名
+            IPHostEntry localhost = Dns.GetHostEntry(hostName);    //方法已过期，可以获取IPv4的地址
+                                                                    //IPHostEntry localhost = Dns.GetHostEntry(hostName);   //获取IPv6地址
+            IPAddress localaddr = localhost.AddressList[1];
+
+            return localaddr.ToString();
+        }
+        static void Main(string[] args)
+        {
+            int port = 6000;
+            string host = GetIpAddress();
+            //string host = "127.0.0.1";
+
+            IPAddress ip = IPAddress.Parse(host);
+            IPEndPoint ipe = new IPEndPoint(ip, port);
+
+            Socket sSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sSocket.Bind(ipe);
+            sSocket.Listen(0);
+            Console.WriteLine("Start listen,please wait...");
+
+            //receive message
+            Socket serverSocket = sSocket.Accept();
+            Console.WriteLine("Connection established!");
+            string recMsg = "";
+            byte[] recbyte = new byte[4096];
+            int bytes = serverSocket.Receive(recbyte, recbyte.Length, 0);
+            recMsg += Encoding.ASCII.GetString(recbyte, 0, bytes);
+            Console.WriteLine(recMsg);
+
+            //send message
+            string sendMsg = "From server:Hello!";
+            byte[] sendByte = Encoding.ASCII.GetBytes(sendMsg);
+            serverSocket.Send(sendByte,sendByte.Length,0);
+            //serverSocket.Close();
+            Console.ReadKey();
+        }
+    }
+}
